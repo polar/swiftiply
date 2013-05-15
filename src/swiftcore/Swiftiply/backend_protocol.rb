04914cec3b99f793ae98a173bc35f54b64f6dcae
Polar Humenn <polar@adiron.com>
2013-05-15 12:12:50 -0400

    Added a "close" protocol
    Added a close protocol for when the Content-Length is not known or is 0. Adds a "X-Swiftiply-Close" header, and the server looks for <!--SC-> before trying to find the next legitmate header. Yep, it's got some problems, especially if somebody puts a <!--SC-> in their data when it's chunked. And it's probably slow having to look for that.

--- src/swiftcore/Swiftiply/backend_protocol.rb
+++ src/swiftcore/Swiftiply/backend_protocol.rb
@@ -61,7 +61,7 @@ module Swiftcore
               @id = preamble[11..22]
               ProxyBag.add_id(self, @id)
               @initialized = true
-              puts "New Backend: #{preamble}"
+              puts "New Backend: #{preamble} #{self}"
             else
               puts "New Backend: Unauthenticated Connection"
               # The worker that connected did not present the proper authentication,
